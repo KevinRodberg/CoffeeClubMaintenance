e0155fc78b8225d0a33e3b43dd762d9d0eaf0e04
Rodberg <krodberg@sfwmd.gov>
2018-05-01 13:48:36 -0400

    Autmoate Endo of Month properties and create ne wmemberlist Also dealt with members 2 month in arears.

--- CoffeeClubMaintenance.R
+++ CoffeeClubMaintenance.R
@@ -19,31 +19,41 @@ eom <-(Vectorize(timeLastDayInMonth))(seq(as.Date(Sys.Date()-365), length=24, by
 
 # eom[12] is last month
 # eom[13] is this month
+MmbrLast2Mon<-subset(CCDues,as.Date(Date) == as.Date((eom[[11]])))
 MmbrLastMon<-subset(CCDues,as.Date(Date) == as.Date((eom[[12]])))
 paidThisMonth <-subset(CCDues,as.Date(Date) == as.Date(eom[[13]]))
 
-MmbrThisMon<-MmbrLastMon[MmbrLastMon$Source == 'Membership' ,]
-               
+MmbrThisMon<-MmbrLast2Mon[MmbrLast2Mon$Source == 'Membership' ,]
+
+MmbrThisMon<-rbind(MmbrThisMon,MmbrLastMon[MmbrLastMon$Source == 'Membership' ,])
+MmbrThisMon<-aggregate(MmbrThisMon$Date,list(MmbrThisMon$Name,MmbrThisMon$Source,MmbrThisMon$DisplayGraph,MmbrThisMon$Paid),max)
+colnames(MmbrThisMon) <- c('Name','Source','DisplayGraph','Paid','Date')
 unPaidMmbrThisMon<- subset(MmbrThisMon,!(MmbrThisMon$Name %in% paidThisMonth$Name))
-unPaidMmbrThisMon$Date<-as.Date("2018-04-30")
+unPaidMmbrThisMon$Date<-as.Date((eom[[13]]))
 unPaidMmbrThisMon$Paid<-0.00
 MmbrThisMonStatus <-rbind(paidThisMonth[paidThisMonth$Source == 'Membership' ,]
                           ,unPaidMmbrThisMon[unPaidMmbrThisMon$Source == 'Membership' ,])
-MemberList <-MmbrThisMonStatus[,c('Name','Date','Paid')]
-CCDues <- rbind(CCDues,unPaidMmbrThisMon)
-latest <- aggregate(CCDues$Date, list(CCDues$Name,CCDues$Source,CCDues$Paid),max)
-colnames(latest) <- c('Name','Source','Paid','Date')
+MemberList <-MmbrThisMonStatus[,c('Name','Source','Paid','Date')]
+
+latest <- aggregate(CCDues$Date, list(CCDues$Name,CCDues$Source,CCDues$DisplayGraph, CCDues$Paid),max)
+colnames(latest) <- c('Name','Source','DisplayGraph','Paid','Date')
 latest$Date <- as.Date(latest$Date)
 paidUntil <-subset(latest,as.Date(latest$Date) > Sys.Date() & latest$Source=='Membership')
+paidUntil<-rbind(subset(MmbrThisMon,!(MmbrThisMon$Name %in% paidThisMonth$Name)),paidUntil)
+paidUntil$Date <- as.Date(paidUntil$Date)
+paidUntil$DisplayGraph <- NULL
 write.csv(paidUntil,"~/Personal/Memberlist.csv")
+
+CCDues <- rbind(CCDues,unPaidMmbrThisMon)
+
 
 CCDues<- CCDues[with(CCDues,order(-DisplayGraph,-xtfrm(Date),Source)),]
 
 #
 # Create Graph for Past Year's Income and Expense
 #
-CCdata<-subset(CCDues,  as.Date(Date) > as.Date(Sys.Date())-365 
-               & as.Date(Date) < as.Date(Sys.Date())+30)
+CCdata<-subset(CCDues,  as.Date(Date) > as.Date(eom[[1]])
+               & as.Date(Date) < as.Date(eom[[14]]))
 CCdata$Date <- as.Date(CCdata$Date)
 reserves <-sum(CCDues$Paid)
 library(scales)
